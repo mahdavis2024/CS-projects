e71c5ed0f77b288c390d615451d6dcec9232034a
mahdavis2024 <m.mahdavisefat@outlook.com>
2024-11-03 10:50:45 +0000

    little edit to EURUSD solution

--- step4/fileprocess.py
+++ step4/fileprocess.py
@@ -34,7 +34,7 @@ def downloading(file_address= 'https://www.histdata.com/download-free-forex-hist
         raise FileDownloadingError(f'An error occured while downloading the file: {er}')
     else:
         print('Your file is here:', Path('file.zip').resolve())
-        print('****File downloaded successfully!')
+        print('****File downloaded successfully!\n')
         return 'file.zip'
 
 
@@ -48,7 +48,7 @@ def unzip(filename= 'HISTDATA_COM_XLSX_EURUSD_M12018.zip'):
         raise FileExtractionError(f'An error occured while extrating the file: {er}')
     else:
         print('Your extracted files are here:', dir_path.resolve())
-        print('****File extracted successfully!')
+        print('****File extracted successfully!\n')
         return dir_path
 
 
@@ -56,15 +56,14 @@ def opening(folderpath = Path.cwd()):
     """opening files one by one and doing some operations on them and closing."""
     try:
         folder_path = folderpath
-        print(folder_path.resolve())
-        print('files in folder')
+        print('files in folder', folder_path.resolve())
         for f in folder_path.iterdir():
             if f.is_file():
                 print('\t',f.name)
     except Exception as er:
         raise PathError(f'An error occured while listing files in folder path: {er}')
     else:
-        print('****Files listed successfully!')
+        print('****Files listed successfully!\n')
         return [f.name for f in folder_path.iterdir()]
 
 def read_write(files_in_folder):
@@ -75,7 +74,7 @@ def read_write(files_in_folder):
             if fp.suffix == '.txt':
                 with open(file, 'r') as infile, open(f'Copy_{fp.name}', 'w') as outfile:
                     for line in infile:
-                        outfile.write(infile.readline())
+                        outfile.write(line)
                     print(fp, 'copied to ', f'Copy_{fp}')
             elif fp.suffix == '.xlsx':
                 with open(file, 'rb') as infile, open(f'Copy_{fp.name}', 'wb') as outfile:
@@ -85,11 +84,11 @@ def read_write(files_in_folder):
     except Exception as er:
         raise FileProcessingError(f'An error occured while processing files: {er}')
     else:
-        print('****Files copied successfully!')
+        print('****Files copied successfully!\n')
 
 def show(files_in_folder):
     """showing the content of excel file. 
-    Because the xlsx in a binary format, it is not easily readable with just buil_in Python functions.
+    Because the xlsx in a binary format, it is not easily readable with just built_in Python functions.
     The xlsx format is essentially a ZIP file, containing XML files that represent the workbooks, sheets, and information.
     Therefore, in this task, we treat the .XLSX as ZIP archive and manually extract the XML data. 
     """
@@ -107,7 +106,7 @@ def show(files_in_folder):
     except Exception as er:
         raise FileProcessingError(f'An error occured while displaying the {file} content: {er}')
     else:
-        print('****File content dispalyed successfully!')
+        print(f'****File content dispalyed successfully!\n')
 
 def find_floats(string):
     # Use regular expression to find all float numbers in the string
@@ -119,7 +118,7 @@ def find_floats(string):
 def float_to_datetime(num):
     time = datetime(1899, 12, 30)
     delta = timedelta(num)
-    return time+delta
+    return time + delta
 
 def showxl(address = Path.cwd()):
     '''displaying the content of the main sheet of the excel file as strings in the output.'''
@@ -137,4 +136,4 @@ def showxl(address = Path.cwd()):
     except Exception as er:
         raise FileProcessingError(f'An error occured while displaying the {xmlpath.name} content: {er}')
     else:
-        print('****File content dispalyed successfully!')                    
+        print('****Sheet content dispalyed successfully!\n')                    
